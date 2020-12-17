0347353b625c7aa9604379b2aed84c49bcc963b8
officeyutong <officeyutong@163.com>
2020-12-17 09:34:57 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Officeyutong/HelloJudge2-Judger
 cd HelloJudge2-Judger
 cd watcher
 g++ -shared -o docker_watcher.so -fPIC -I/usr/include/python3.8 docker_watcher.cpp -lpython3.8 -lboost_python38 -Wall -Wextra -O3
-co docker_watcher.so ..
+cp docker_watcher.so ..
 cd ..
 pip3 install -r requirements.txt
 cd docker
