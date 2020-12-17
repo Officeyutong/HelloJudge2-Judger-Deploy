75a6f604de2548b0b4ae497118fc2d312aedf53c
officeyutong <officeyutong@163.com>
2020-12-17 19:17:58 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ apt install -y docker.io git libboost-python-dev python3 python3-pip g++ python3
 git clone https://github.com/Officeyutong/HelloJudge2-Judger
 cd HelloJudge2-Judger
 cd watcher
-g++ -shared -o docker_watcher.so -fPIC -I/usr/include/python3.8 docker_watcher.cpp -lpython3.8 -lboost_python38 -Wall -Wextra -O3
+g++ -shared -o docker_watcher.so -fPIC -I/usr/include/python3.8 docker_watcher.cpp -lpython3.8 -lboost_python3 -Wall -Wextra -O3
 cp docker_watcher.so ..
 cd ..
 pip3 install -r requirements.txt
